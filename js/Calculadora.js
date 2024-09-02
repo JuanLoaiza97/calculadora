ed8816b42eb8c8367a49a674ea72dc78ff631aad
juanLoaiza <jloaiza097@gmail.com>
2024-09-02 17:17:36 -0500

    agregar proyecto calculadora js css

--- js/Calculadora.js
+++ js/Calculadora.js
@@ -8,7 +8,15 @@ class Calculadora {
     }
 
     multiplicar(num1, num2){
-        return num1 * num2;
+        if(num2 == 0){
+            return 0
+        } else if (num1 == 0){
+            return 0
+        }else{
+            return num1 * num2;
+
+        }
+        
     }
 
     dividir(num1, num2){
